20a2cc37f1aea83490e9d2df3873a1ef766adce7
Pedro-PFerreira <pp.mp.ferreira@gmail.com>
2024-02-28 19:30:35 +0000

    Added the logic for 3 power-ups

--- scenes/entities/player/player.gd
+++ scenes/entities/player/player.gd
@@ -4,6 +4,8 @@ const SPEED = 100.0
 const JUMP_VELOCITY = -400.0
 var DIRECTION = Vector2.RIGHT
 var mouse_in = false
+var gravity_modifier = 1.0
+var speed_modifier = 1.0
 
 # Get the gravity from the project settings to be synced with RigidBody nodes.
 var gravity = ProjectSettings.get_setting("physics/2d/default_gravity")
@@ -15,23 +17,21 @@ func _ready():
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
-		velocity.y += gravity * delta
-	
+		velocity.y += gravity * gravity_modifier * delta
+
 	if is_on_wall():
-		DIRECTION *= -1
+		DIRECTION.x *= -1
 		$Sprite2D.flip_h = not $Sprite2D.flip_h
 
-	velocity = DIRECTION * SPEED
+	if is_on_floor() and Input.is_action_just_pressed("Click") and mouse_in == true:
+		process_powerup("weight", delta)
 
-	if is_on_floor() and Input.is_action_pressed("Click") and mouse_in == true:
-		process_powerup("jump", delta)
-
+	velocity.x = DIRECTION.x * SPEED * speed_modifier
 
 	move_and_slide()
 
 func animate():
 	# Tween to change sprite frame from 1 to 8.
-
 	var tween = get_tree().create_tween().set_loops()
 	tween.tween_property($Sprite2D, "frame", 7, 1).from(0)
 
@@ -40,22 +40,18 @@ func process_powerup(powerup, _delta):
 	if powerup == "jump":
 		velocity.y = JUMP_VELOCITY
 	elif powerup == "flip":
-		# DIRECTION *= -1
-		# $Sprite2D.flip_h = not $Sprite2D.flip_h
-		pass
+		DIRECTION.x *= -1
+		$Sprite2D.flip_h = not $Sprite2D.flip_h
 	elif powerup == "weight":
-		# velocity.x *= 0.5
-		# velocity.y += gravity * 4 * delta
-		pass
+		speed_modifier = 0.5
+		gravity_modifier = 5.0
 	else:
 		# error powerup desconhecido
 		pass
 		
 
 func _on_mouse_entered():
-	print("Entering")
 	mouse_in = true
-	process_powerup("jump", 0.0)
 
 
 func _on_mouse_exited():
