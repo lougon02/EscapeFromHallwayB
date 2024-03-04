d43e158eb9ae1879b2afbd6a34c7737d6cbbf645
Lourenço Gonçalves <lourencogon02@gmail.com>
2024-03-04 12:36:55 +0000

    Added movable camera

--- scenes/levels/level.gd
+++ scenes/levels/level.gd
@@ -5,13 +5,19 @@ var freshman = preload("res://scenes/entities/player/player.tscn")
 var spawn_count = 4  # Number of instances to spawn
 var spawned_count = 0  # Tracks spawned instances
 
+func _process(_delta):
+	if Input.is_action_pressed("Left"):
+		$Camera2D.position.x -= 8
+	if Input.is_action_pressed("Right"):
+		$Camera2D.position.x += 8
+
 # Spawn of freshmen
 func _on_spawn_timer_timeout():
 	
 	if spawned_count < spawn_count:
 		var freshman_instance = freshman.instantiate()
 		add_child(freshman_instance)
-		freshman_instance.position = $SpawnMarker.position
+		freshman_instance.position = $Spawner/SpawnMarker.position
 		spawned_count += 1
 		# Create the signals connection
 		freshman_instance.connect("has_jumped", _on_player_has_jumped)
