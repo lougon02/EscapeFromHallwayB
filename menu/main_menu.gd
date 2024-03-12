9c47fe83f8bd12c7c6663c2b55286603a46c4e94
Pedro-PFerreira <pp.mp.ferreira@gmail.com>
2024-03-12 12:44:33 +0000

    Created instructions menu

--- menu/main_menu.gd
+++ menu/main_menu.gd
@@ -4,3 +4,6 @@ func _on_play_button_pressed() -> void:
 	$MainMenuComponents/AnimationPlayer.play("start_game_transition")
 	await $MainMenuComponents/AnimationPlayer.animation_finished
 	get_tree().change_scene_to_file("res://scenes/levels/level.tscn")
+
+func _on_instructions_button_pressed():
+	get_tree().change_scene_to_file("res://menu/instructions_menu.tscn")
